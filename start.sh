01d358c5434f4b55175e306ab46ad05165eaa183
Martijn van Maurik <martijn@vmaurik.nl>
2015-05-04 13:03:11 +0200

    Fix some issues

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
 echo "Starting mini-dinstall..."
 
-mini-dinstall
+if [[ ! -e "/data/mini-dinstall.conf" ]]
+then
+    cp /etc/mini-dinstall.conf /data/mini-dinstall.conf
+fi
+
 nginx
 
-chmod 1777 /data/mini-dinstall/incoming
+mini-dinstall --config=/data/mini-dinstall.conf
 
 tail -f /data/mini-dinstall/mini-dinstall.log
